ceb45634c3b9c40db602c193089884613b7c9fbe
Franck Royer <royer.franck@gmail.com>
2016-04-19 14:20:57 +0100

    moving forward with Flask

---
+++ config.py
@@ -0,0 +1,9 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = '2c6c6b1028695e0ca069935eb6b77cfb494f5e04'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
